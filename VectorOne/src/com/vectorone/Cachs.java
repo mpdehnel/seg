f56c3db403a1ee9018171c38d2cdfd1fa699d399
robertketelhut-Mac <robertketelhut@yahoo.de>
2012-12-20 13:06:13 +0100

    Project Commit
    Firtst LayoutCommit with back history

---
+++ VectorOne/src/com/vectorone/Cachs.java
@@ -0,0 +1,13 @@
+package com.vectorone;
+
+public class Cachs {
+
+	private double distance;
+	private String discripton;
+	private double direction;
+	private int teamcolor;
+	
+	
+	
+	
+}
---
+++ VectorOne/src/com/vectorone/LoginValidation.java
@@ -0,0 +1,19 @@
+package com.vectorone;
+
+public class LoginValidation {
+
+	public boolean isfound;
+
+	public LoginValidation() {
+		// TODO ServerConnection Database 
+	}
+
+	public boolean isUser(String username, String password) {
+		
+		//TODO if user is valid than true 
+		
+		
+		return true;
+	}
+
+}
